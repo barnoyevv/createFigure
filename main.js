7dab6d909be15a3098598fef196945db0697341c
Shohjahon <shohjahon@MacBook-Air-Shohjahon.local>
2024-05-02 05:57:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const create =(e)=>{
+	e.preventDefault();
+	let result = document.getElementById("result")
+	let width = e.target[0].value
+	let height = e.target[1].value
+	let radius = e.target[2].value
+	let color = e.target[3].value
+	console.log(width, height, radius, color);
+	result.style.width = `${width}px`
+	result.style.height = `${height}px`
+	result.style.borderRadius = `${radius}px`
+	result.style.backgroundColor = color
+}
